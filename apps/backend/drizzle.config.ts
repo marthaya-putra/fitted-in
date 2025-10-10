e72102c39e859eb507922afb6a08e52c6f7195be
Marthayaputra <marthayaputra@hotmail.com>
2025-10-10 18:48:58 +0800

    chore: update database connection details and improve README formatting
    - Changed the database connection URL in drizzle.config.ts for local development.
    - Reformatted the README.md to enhance clarity and consistency in setup instructions and examples.

--- apps/backend/drizzle.config.ts
+++ apps/backend/drizzle.config.ts
@@ -1,17 +1,17 @@
-import { defineConfig } from 'drizzle-kit';
-import * as dotenv from 'dotenv';
+import { defineConfig } from "drizzle-kit";
+import * as dotenv from "dotenv";
 
 // Load environment variables
 dotenv.config();
 
 export default defineConfig({
-  schema: './src/db/schema/index.ts',
-  out: './drizzle',
-  dialect: 'postgresql',
+  schema: "./src/db/schema/index.ts",
+  out: "./drizzle",
+  dialect: "postgresql",
   dbCredentials: {
     url:
       process.env.DATABASE_URL ||
-      'postgresql://postgres:password@localhost:5430/nestdb',
+      "postgresql://postgres:postgres@localhost:5434/fitted-in",
   },
   verbose: true,
   strict: true,
