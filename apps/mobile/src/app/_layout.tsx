913e78f710f3fafc23a7a5d0b7869b3bda2047cd
Marthayaputra <marthayaputra@hotmail.com>
2026-03-05 14:16:55 +0700

    feat: add mobile app

---
+++ apps/mobile/src/app/_layout.tsx
@@ -0,0 +1,27 @@
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import React from "react";
+import { useColorScheme } from "react-native";
+
+import { AnimatedSplashOverlay } from "@/components/animated-icon";
+import AppTabs from "@/components/app-tabs";
+
+import "./global.css";
+
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+import "@/src/app/global.css";
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+  return (
+    <GluestackUIProvider mode="system">
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        <AnimatedSplashOverlay />
+        <AppTabs />
+      </ThemeProvider>
+    </GluestackUIProvider>
+  );
+}
