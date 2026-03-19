e69dbc72695845ff9aec996cfce62ddea5ba648a
Marthayaputra <marthayaputra@hotmail.com>
2026-03-19 16:50:52 +0700

    fix: update base URL in auth client to use NEXT_PUBLIC_APP_HOST

--- apps/admin/src/lib/auth-client.ts
+++ apps/admin/src/lib/auth-client.ts
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 
-const base = process.env.NEXT_PUBLIC_API_URL;
-console.log("BASE URL FROM NEXT_PUBLIC_API_URL: ", base);
+const base = process.env.NEXT_PUBLIC_APP_HOST;
+console.log("BASE URL FROM NEXT_PUBLIC_APP_HOST: ", base);
 export const authClient = createAuthClient({
   baseURL: base,
   fetchOptions: {
